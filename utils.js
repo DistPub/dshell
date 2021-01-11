912b5ed15885cdc595a9bb6d9079e7427af05a2c
Smite Chow <xiaopengyou2no1@gmail.com>
2021-01-11 16:56:55 +0800

    add sleep

--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
 export function log(text) {
   console.log(text)
 }
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 export const AsyncFunction = (async () => {}).constructor
 export const AsyncGeneratorFunction = (async function* () {}).constructor
 export const GeneratorFunction = (function* () {}).constructor
